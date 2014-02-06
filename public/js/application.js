a48118628d33d0365aeea841f83d36d35aca3c60
Kevin Falank <kevinf@carolina.rr.com>
2014-02-06 10:21:48 -0600

    working mvp

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+  $("#get_color").click(function(e) {
+    $.post( "/color", function( data ) {
+      console.log (data.cell);
+      console.log (data.color);
+      $("#color_me li:nth-child(" + data.cell + ")").css("background-color",data.color);
+    },"json");
 
+  });
 });
